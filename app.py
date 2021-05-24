703cfba162a1e221075f607d6abcc9b66e1663e8
dhruv8609 <dhruv.sh2017@gmail.com>
2021-05-24 15:37:49 +0530

    added summarizer

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 from flask import Flask, request, render_template
 from youtube_transcript_api import YouTubeTranscriptApi
-# define a variable to hold you app
+from transformers import pipeline
+
 app = Flask(__name__)
 
-# define your resource endpoints
+
+model_path = './summarizer_model'
+
+
+def return_summary(text):
+    model = pipeline('summarization', model=model_path, tokenizer=model_path)
+    length = len(text)
+    summary = []
+    for i in range(0, num_iters + 1):
+        start = 0
+        start = i * 2000
+        end = (i + 1) * 2000
+        res = model(text[start:end])
+        res = res[0]
+        res = res['summary_text']
+        summary.append(res)
+    return summary
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -14,13 +31,12 @@ def home_page():
         script = YouTubeTranscriptApi.get_transcript(videoid)
         for segment in script:
             subtitle = subtitle + ' ' + segment['text']
-        return render_template('index.html', post=subtitle)
+        vid_summary = return_summary(subtitle)
+        return render_template('index.html', post=vid_summary)
 
     elif request.method == "GET":
         return render_template('index.html', post=subtitle)
 
 
-
-# server the app when this file is run
 if __name__ == '__main__':
     app.run(debug=True)
