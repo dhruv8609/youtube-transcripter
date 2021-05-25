90b580c0713f3b9bf6f4591ff16fcd8073e2fea7
dhruv8609 <dhruv.sh2017@gmail.com>
2021-05-25 15:01:50 +0530

    API done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from youtube_transcript_api import YouTubeTranscriptApi
 from transformers import pipeline
 
@@ -8,10 +8,12 @@ app = Flask(__name__)
 model_path = './summarizer_model'
 
 
-def return_summary(text):
-    model = pipeline('summarization', model=model_path, tokenizer=model_path)
+def get_summary(text):
+    model = pipeline('summarization', model=model_path,
+                     tokenizer=model_path)
     length = len(text)
     summary = []
+    num_iters = int(length / 2000)
     for i in range(0, num_iters + 1):
         start = 0
         start = i * 2000
@@ -23,19 +25,43 @@ def return_summary(text):
     return summary
 
 
-@app.route('/', methods=["GET", "POST"])
-def home_page():
+def get_transcript(videoid):
+    script = YouTubeTranscriptApi.get_transcript(videoid)
     subtitle = ''
-    if request.method == "POST":
-        videoid = request.form.get('vid-id', '')
-        script = YouTubeTranscriptApi.get_transcript(videoid)
-        for segment in script:
-            subtitle = subtitle + ' ' + segment['text']
-        vid_summary = return_summary(subtitle)
-        return render_template('index.html', post=vid_summary)
+    for segment in script:
+        subtitle = subtitle + ' ' + segment['text']
+    return subtitle
 
-    elif request.method == "GET":
-        return render_template('index.html', post=subtitle)
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    return "Home Page"
+
+
+# @app.route('/run_next/', methods=["GET", "POST"])
+# def summarization():
+#     subtitle = ''
+#     rrtt = 'An article is any member of a class of dedicated words that are used with noun phrases to mark the identifiability of the referents of the noun phrases. The category of articles constitutes a part of speech.'
+#     if request.method == "POST":
+#         videoid = request.form.get('vid-id', '')
+#         transcript = get_transcript(videoid)
+#         vid_summary = get_summary(subtitle)
+#         return render_template('index.html', post=vid_summary)
+
+#     elif request.method == "GET":
+#         return render_template('index.html', post=subtitle)
+
+
+@app.route('/api/summarize', methods=["GET"])
+def home_function():
+    url = request.args.get('youtube_url')
+    video_id = url.split("=")[1]
+    try:
+        transcript = get_transcript(video_id)
+        summary_final = get_summary(transcript)
+        return jsonify({'url': transcript, 'summary': summary_final})
+    except:
+        return jsonify({'url': 'ERROR:Check the URL or try again with different URL'})
 
 
 if __name__ == '__main__':
